83ce3f7785d5b0cec9d9f7969874154c25d27360
João <Joo@DESKTOP-6KGDSH8>
2016-10-28 16:38:56 -0200

    teste jpa
    Change-Id: I51e82d9e31fa7eec90f7ed66bb67ca7b07236078
    Signed-off-by: João <João@DESKTOP-6KGDSH8>

---
+++ springboot-jpa/src/controller/Application.java
@@ -0,0 +1,18 @@
+package controller;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import repository.PessoaRepository;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class);
+	}
+	
+	
+}
