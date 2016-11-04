f8c95bb6ead34c47b9d55e7894a81ad37a98cc94
Joao Cesar <joaocesarfernandes@gmail.com>
2016-11-04 15:51:35 -0200

    modificaocao

--- rest-teste/src/main/java/com/genoa/controller/GenoaController.java
+++ rest-teste/src/main/java/com/genoa/controller/GenoaController.java
@@ -3,13 +3,10 @@ package com.genoa.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.genoa.api.version.ApiVersion;
 import com.genoa.model.Usuario;
 import com.genoa.repository.UsuarioRepository;
 
